174c5cb5939f47b734234f0c009245f298cd7c2f
Fernando Panduro <92656447+fernandopanduro@users.noreply.github.com>
2024-02-05 21:23:49 -0700

    ✨ feat: Add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,40 @@ import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Components Glassmorphism",
-  description: "Componens Glassmorphism",
+  icons: {
+    icon: "https://glassmorphism-components.vercel.app/favicon.png",
+  },
+  title: "Collection of Glassmorphism components and effects.",
+  description: "Collection of Glassmorphism Components and effects.",
+  keywords:
+    "Collection of Glassmorphism Components, components react, shadcn, materialui, glassmorphism, glassmorphism ciomponents",
+  applicationName: "Collection of Glassmorphism Components.",
+  authors: [{ name: "Fernando Panduro", url: "https://ferpanduro.com" }],
+  creator: "Fernando Panduro",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    url: "https://glassmorphism-components.vercel.app/",
+    title: "Collection of Glassmorphism components and effects.",
+    description:
+      "Collection of glassmorphism components and effects. Crafted with React and Tailwind CSS.",
+    siteName: "Collection of Glassmorphism Components.",
+    images: [
+      {
+        url: "https://glassmorphism-components.vercel.app/cube.jpg",
+        alt: "Collection of Glassmorphism Components.",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    // site: "@tucuenta", // Reemplaza con tu nombre de usuario de Twitter
+    // creator: "@tucuenta", // Reemplaza con tu nombre de usuario de Twitter
+    title: "Collection of Glassmorphism Components and effects.",
+    description:
+      "Collection of glassmorphism components and effects. Crafted with React and Tailwind CSS.",
+    images: "https://glassmorphism-components.vercel.app/cube.jpg",
+  },
 };
 
 export default function RootLayout({
