1c85b12f5cddeac8f463b028a1e94c5e1941f467
Fernando Panduro <92656447+fernandopanduro@users.noreply.github.com>
2024-02-05 21:25:40 -0700

    ✨ feat: Add analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +53,7 @@ export default function RootLayout({
       <body className={`relative ${inter.className}`}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
