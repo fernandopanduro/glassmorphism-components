52bb82f881e7a70e407dfc4589f29ac23794f33e
Fernando Panduro <92656447+fernandopanduro@users.noreply.github.com>
2024-02-10 11:03:52 -0700

    ✨ feat: Update form component

--- components/lab/form/form.tsx
+++ components/lab/form/form.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 type Props = {};
 
-const Form = (props: Props) => {
+const page = (props: Props) => {
+  const [inputValueUsername, setInputValueUsername] = useState("");
+  const [inputValuePassword, setInputValuePassword] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <section className="p-28 grid place-items-center bg-gradient-to-r from-violet-200 to-pink-200 relative min-h-svh overflow-hidden">
       <div className="absolute -top-96 w-[600px] h-[600px] bg-pink-500 blur-[250px]"></div>
@@ -16,44 +24,65 @@ const Form = (props: Props) => {
           <div className="animate-form-square z-0 delay-500 absolute -bottom-20 -left-32 h-5 w-5 backdrop-blur-sm bg-[rgba(255,255,255,0.1)] shadow-xl shadow-[rgba{0,0,0,0.1}] border-r border-b border-[rgba{255,255,255,0.5}] rounded-lg"></div>
           <div className="animate-form-square z-0 delay-300 absolute -top-20 left-36 h-16 w-16 backdrop-blur-sm bg-[rgba(255,255,255,0.1)] shadow-xl shadow-[rgba{0,0,0,0.1}] border-r border-b border-[rgba{255,255,255,0.5}] rounded-lg"></div>
           <form
-            action=""
+            onSubmit={onSubmit}
             className="relative w-full h-full p-7 z-50 backdrop-blur-lg">
-            <legend className="text-white text-2xl font-bold leading-6 mb-10">
-              Login Form
-            </legend>
-            <div className="w-full mt-5">
-              <input
-                type="text"
-                placeholder="Username"
-                className="w-full bg-[rgba(255,255,255,.2)] rounded-full border-none outline-none px-4 py-2 border-r-0 border-[rgba(255,255,255,0.2)] text-base text-white shadow-lg shadow-[rgba(0,0,0,0.05)] placeholder:text-white"
-              />
-            </div>
-            <div className="w-full mt-5">
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full bg-[rgba(255,255,255,.2)] rounded-full border-none outline-none px-4 py-2 border-r-0 border-[rgba(255,255,255,0.2)] text-base text-white shadow-lg shadow-[rgba(0,0,0,0.05)] placeholder:text-white"
-              />
-            </div>
-            <div className="w-full mt-5">
-              <input
-                type="submit"
-                value="Login"
-                className="w-full bg-white rounded-full border-none outline-none px-4 py-2 border-r-0 border-[rgba(255,255,255,0.2)] text-base  text-slate-500 cursor-pointer max-w-28 shadow-lg shadow-[rgba(0,0,0,0.05)] placeholder:text-white mb-5 font-bold"
-              />
-            </div>
-            <p className="text-white mt-5">
-              Forgot Password?{" "}
-              <a href="#" className="font-bold">
-                Click Here
-              </a>
-            </p>
-            <p className="text-white mt-5">
-              Dont have an account?{" "}
-              <a href="#" className="font-bold">
-                Sing Up
-              </a>
-            </p>
+            <fieldset className="flex flex-col gap-6">
+              <legend className="text-white text-2xl font-bold leading-6 mb-6">
+                Login Form
+              </legend>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <div className="w-full ">
+                    <input
+                      tabIndex={1}
+                      type="text"
+                      placeholder="Username"
+                      required
+                      autoFocus
+                      autoComplete="username"
+                      value={inputValueUsername}
+                      onChange={e => setInputValueUsername(e.target.value)}
+                      className="w-full bg-[rgba(255,255,255,.2)] rounded-full border-none outline-none px-4 py-2 border-r-0 border-[rgba(255,255,255,0.2)] text-base text-white shadow-lg shadow-[rgba(0,0,0,0.05)] placeholder:text-white focus:outline focus:outline-blue-600"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <input
+                      tabIndex={2}
+                      type="password"
+                      placeholder="Password"
+                      required
+                      alt="current-password"
+                      value={inputValuePassword}
+                      onChange={e => setInputValuePassword(e.target.value)}
+                      className="w-full bg-[rgba(255,255,255,.2)] rounded-full border-none outline-none px-4 py-2 border-r-0 border-[rgba(255,255,255,0.2)] text-base text-white shadow-lg shadow-[rgba(0,0,0,0.05)] placeholder:text-white focus:outline focus:outline-blue-600"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full">
+                  <input
+                    tabIndex={3}
+                    type="submit"
+                    value="Login"
+                    className="w-full bg-white rounded-full border-none outline-none px-4 py-2 border-r-0 border-[rgba(255,255,255,0.2)] text-base  text-slate-500 cursor-pointer max-w-28 shadow-lg shadow-[rgba(0,0,0,0.05)] placeholder:text-white font-bold focus:outline focus:outline-blue-600"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-white ">
+                  Forgot Password?{" "}
+                  <a href="#" className="font-bold">
+                    Click Here
+                  </a>
+                </p>
+                <p className="text-white ">
+                  Dont have an account?{" "}
+                  <a href="#" className="font-bold">
+                    Sing Up
+                  </a>
+                </p>
+              </div>
+            </fieldset>
           </form>
         </div>
       </div>
@@ -61,4 +90,4 @@ const Form = (props: Props) => {
   );
 };
 
-export default Form;
+export default page;
