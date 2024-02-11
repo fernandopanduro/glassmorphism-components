7fd5380da797a49cfa0f427a12b4f33abe47ac9c
Fernando Panduro <92656447+fernandopanduro@users.noreply.github.com>
2024-02-10 20:36:34 -0700

    🐞 fix: update name file

--- data/components.ts
+++ data/components.ts
@@ -16,7 +16,7 @@ import BadgeAnimatedGradientBorder from "@/components/lab/badge/BadgeAnimatedGra
 import Reviews from "@/components/lab/section/Review";
 import FormGlassmorphism from "@/components/lab/form/FormGlassmorphism";
 import CardPresentation from "@/components/lab/section/CardPresentation";
-import CardPresentationGlassmorphism from "@/components/lab/section/cardPresentationGlassmorphism";
+import CardPresentationGlassmorphism from "@/components/lab/section/CardPresentationGlassmorphism";
 const TWCONFIG = {
   ["text-gradient"]: {
     animation: {
